25ed3984215e58e8d6436908c67d7c102bdb25e4
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-21 13:50:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let closest = 0;
+	for(let i =0; i<arr.length; i++){
+		let sum = 0;		
+		for(let j = i; j<i+3; j++)
+			{
+				sum += arr[j];
+			}
+		if(sum == target)
+			return sum;
+		else 
+			closest = Math.min(c, sum);			
+	}
+  return closest;
 }
 
 module.exports = threeSum;
