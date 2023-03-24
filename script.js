524cdef41bfca7b4a583c891b03f29ae5e422d29
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-24 17:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function threeSum(arr, target) {
 // write your code here
 	let closest = 10000;
-	for(let i =0; i<arr.length; i++){
+	for(let i =0; i<arr.length-3; i++){
 		let sum = 0;		
 		for(let j = i; j<i+3; j++)
 			{
 				sum += arr[j];
 			}
+		
 		if(sum == target)
 			return sum;
 		else 
